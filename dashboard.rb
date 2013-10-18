1e424b98f43c6d924db9db3397e61904d51697b6
Jordan Hatch <jordan@jordanh.net>
2013-10-18 19:14:26 +0100

    Make both sets of basic auth credentials optional

--- dashboard.rb
+++ dashboard.rb
@@ -10,8 +10,30 @@ require 'presenters/group_presenter'
 
 module SituationRoom
   class Dashboard < Sinatra::Base
-    use Rack::Auth::Basic, "Please login to this dashboard" do |username, password|
-      username == ENV['SITUATION_ROOM_DASHBOARD_USER'] and password == ENV['SITUATION_ROOM_DASHBOARD_PASSWORD']
+
+    configure do
+      set :api_endpoint, ENV["SITUATION_ROOM_API_ENDPOINT"]
+      set :api_user, ENV["SITUATION_ROOM_API_USER"]
+      set :api_password, ENV["SITUATION_ROOM_API_PASSWORD"]
+
+      set :app_user, ENV['SITUATION_ROOM_DASHBOARD_USER']
+      set :app_password, ENV['SITUATION_ROOM_DASHBOARD_PASSWORD']
+
+      if settings.app_user? && settings.app_password?
+        use Rack::Auth::Basic, "Please login to this dashboard" do |username, password|
+          username == settings.app_user and password == settings.app_password
+        end
+      end
+    end
+
+    helpers do
+      def situation_room_api(options = {})
+        if settings.api_user? && settings.api_password?
+          options.merge!(user: settings.api_user, password: settings.api_password)
+        end
+
+        @client ||= ApiClient.new(settings.api_endpoint, options)
+      end
     end
 
     get '/' do
@@ -30,10 +52,6 @@ module SituationRoom
     get '/group/:id' do
       @group = params[:id]
       erb :dashboard
-    end
-
-    def situation_room_api
-      @client ||= ApiClient.new(ENV["SITUATION_ROOM_API_ENDPOINT"], user: ENV["SITUATION_ROOM_API_USER"], password: ENV["SITUATION_ROOM_API_PASSWORD"])
     end
   end
 end
